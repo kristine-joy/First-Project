1bc8ed52b5d061c5092695a7f2ee7bad4030a537
Aldogsss05 <aldrinmel20@gmail.com>
2026-03-20 12:03:28 +0800

    My First Project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "Kristine joy";
+    $role = "Student";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Profile</title>
+</head>
+<body>
+    <h1>Hello, my name is <?php echo $name; ?>!</h1>
+    <p>I am a <?php echo $role; ?>.</p>
+</body>
+</html>
